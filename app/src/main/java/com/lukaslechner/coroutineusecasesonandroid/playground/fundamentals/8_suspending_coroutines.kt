ab6ce083c86fb1ebdd546a10f5bacc9de1d01ca9
lukaslechner <office@lukaslechner.com>
2020-05-16 12:23:29 +0200

    Add example "suspending coroutine"

---
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/fundamentals/8_suspending_coroutines.kt
@@ -0,0 +1,27 @@
+package com.lukaslechner.coroutineusecasesonandroid.playground.fundamentals
+
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.joinAll
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+    println("main starts")
+    joinAll(
+        async { suspendingCoroutine(1, 500) },
+        async { suspendingCoroutine(2, 300) },
+        async {
+            repeat(5) {
+                println("other tasks is working on ${Thread.currentThread().name}")
+                delay(100)
+            }
+        }
+    )
+    println("main ends")
+}
+
+suspend fun suspendingCoroutine(number: Int, delay: Long) {
+    println("Coroutine $number starts work on ${Thread.currentThread().name}")
+    delay(delay)
+    println("Coroutine $number has finished on ${Thread.currentThread().name}")
+}
