c32f53b5ca4a3cca3a6e6500b19c0c7d2dcea21a
lukaslechner <office@lukaslechner.com>
2020-06-09 11:33:56 +0200

    Improve Readability of UseCase#8

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase8/RoomAndCoroutinesViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase8/RoomAndCoroutinesViewModel.kt
@@ -7,32 +7,31 @@ import kotlinx.coroutines.launch
 
 class RoomAndCoroutinesViewModel(
     private val api: MockApi,
-    var database: AndroidVersionDao
+    private val database: AndroidVersionDao
 ) : BaseViewModel<UiState>() {
 
     fun loadData() {
         uiState.value = UiState.Loading.LoadFromDb
 
         viewModelScope.launch {
-
             val localVersions = database.getAndroidVersions()
-            if (localVersions.isNotEmpty()) {
-                uiState.value =
-                    UiState.Success(DataSource.DATABASE, localVersions.mapToUiModelList())
-            } else {
+            if (localVersions.isEmpty()) {
                 uiState.value =
                     UiState.Error(DataSource.DATABASE, "Database empty!")
+            } else {
+                uiState.value =
+                    UiState.Success(DataSource.DATABASE, localVersions.mapToUiModelList())
             }
 
             uiState.value = UiState.Loading.LoadFromNetwork
             try {
                 val recentVersions = api.getRecentAndroidVersions()
-                for (recentVersion in recentVersions) {
-                    database.insert(recentVersion.mapToEntity())
+                for (version in recentVersions) {
+                    database.insert(version.mapToEntity())
                 }
                 uiState.value = UiState.Success(DataSource.NETWORK, recentVersions)
             } catch (exception: Exception) {
-                uiState.value = UiState.Error(DataSource.NETWORK, "Network Request failed")
+                uiState.value = UiState.Error(DataSource.NETWORK, "Something went wrong!")
             }
         }
     }
