6cf22a4c6e688a03c02e39b95aa4c7af54ccb720
lukaslechner <office@lukaslechner.com>
2020-06-10 10:40:34 +0200

    Clear implementations of first three exercises for coroutines course

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/Perform2SequentialNetworkRequestsViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/Perform2SequentialNetworkRequestsViewModel.kt
@@ -1,28 +1,14 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase2
 
-import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
-import kotlinx.coroutines.launch
 
 class Perform2SequentialNetworkRequestsViewModel(
     private val mockApi: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun perform2SequentialNetworkRequest() {
-        uiState.value = UiState.Loading
-        viewModelScope.launch {
-            try {
-                val recentVersions = mockApi.getRecentAndroidVersions()
-                val mostRecentVersion = recentVersions.last()
-
-                val featuresOfMostRecentVersion =
-                    mockApi.getAndroidVersionFeatures(mostRecentVersion.apiLevel)
-
-                uiState.value = UiState.Success(featuresOfMostRecentVersion)
-            } catch (exception: Exception) {
-                uiState.value = UiState.Error("Network Request failed")
-            }
-        }
+        // TODO: Exercise 1
+        // switch to branch "coroutine_course_full" to see solution
     }
 }
--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase7/TimeoutAndRetryViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase7/TimeoutAndRetryViewModel.kt
@@ -1,10 +1,7 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase7
 
-import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
-import kotlinx.coroutines.*
-import timber.log.Timber
 
 class TimeoutAndRetryViewModel(
     private val api: MockApi = mockApi()
@@ -15,57 +12,9 @@ class TimeoutAndRetryViewModel(
         val numberOfRetries = 2
         val timeout = 1000L
 
-        val oreoVersionsDeferred = viewModelScope.async {
-            retryWithTimeout(numberOfRetries, timeout) {
-                api.getAndroidVersionFeatures(27)
-            }
-        }
+        // TODO: Exercise 3
+        // switch to branch "coroutine_course_full" to see solution
 
-        val pieVersionsDeferred = viewModelScope.async {
-            retryWithTimeout(numberOfRetries, timeout) {
-                api.getAndroidVersionFeatures(28)
-            }
-        }
-
-        viewModelScope.launch {
-            try {
-                val versionFeatures = listOf(
-                    oreoVersionsDeferred,
-                    pieVersionsDeferred
-                ).awaitAll()
-
-                uiState.value = UiState.Success(versionFeatures)
-
-            } catch (e: Exception) {
-                Timber.e(e)
-                uiState.value = UiState.Error("Network Request failed")
-            }
-        }
-    }
-
-    private suspend fun <T> retryWithTimeout(
-        numberOfRetries: Int,
-        timeout: Long,
-        block: suspend () -> T
-    ) = retry(numberOfRetries) {
-        withTimeout(timeout) {
-            block()
-        }
-    }
-
-    private suspend fun <T> retry(
-        numberOfRetries: Int,
-        delayBetweenRetries: Long = 100,
-        block: suspend () -> T
-    ): T {
-        repeat(numberOfRetries) {
-            try {
-                return block()
-            } catch (exception: Exception) {
-                Timber.e(exception)
-            }
-            delay(delayBetweenRetries)
-        }
-        return block() // last attempt
+        // run api.getAndroidVersionFeatures(27) and api.getAndroidVersionFeatures(28) in parallel
     }
 }
