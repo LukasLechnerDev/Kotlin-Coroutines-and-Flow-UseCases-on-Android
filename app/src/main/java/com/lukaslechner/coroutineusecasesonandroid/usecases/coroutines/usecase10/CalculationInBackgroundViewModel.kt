8eb10b3578a873a3e799ff29551f10eda3fad767
lukaslechner <office@lukaslechner.com>
2020-07-15 14:29:33 +0200

    Remove dispatcher from constructor of CalculationInBackgroundViewModel.kt

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase10/CalculationInBackgroundViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase10/CalculationInBackgroundViewModel.kt
@@ -1,12 +1,8 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase10
 
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
 
-class CalculationInBackgroundViewModel(
-    private val defaultDispatcher: CoroutineDispatcher = Dispatchers.Default
-) : BaseViewModel<UiState>() {
+class CalculationInBackgroundViewModel : BaseViewModel<UiState>() {
 
     fun performCalculation(factorialOf: Int) {
 
