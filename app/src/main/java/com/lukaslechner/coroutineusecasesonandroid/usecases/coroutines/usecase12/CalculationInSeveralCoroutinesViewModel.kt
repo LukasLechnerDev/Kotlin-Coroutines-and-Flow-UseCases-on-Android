421ca3d95b7030fee8047561df91ab1b9804f997
lukaslechner <office@lukaslechner.com>
2020-07-21 11:43:35 +0200

    Prepare exercise#4

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase12/CalculationInSeveralCoroutinesViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase12/CalculationInSeveralCoroutinesViewModel.kt
@@ -3,6 +3,8 @@ package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase1
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import java.math.BigInteger
+import kotlin.system.measureTimeMillis
 
 class CalculationInSeveralCoroutinesViewModel(
     private val factorialCalculator: FactorialCalculator = FactorialCalculator(),
@@ -13,6 +15,26 @@ class CalculationInSeveralCoroutinesViewModel(
         factorialOf: Int,
         numberOfCoroutines: Int
     ) {
+        uiState.value = UiState.Loading
 
+        var factorialResult = BigInteger.ZERO
+        val computationDuration = measureTimeMillis {
+            factorialResult =
+                factorialCalculator.calculateFactorial(
+                    factorialOf,
+                    numberOfCoroutines
+                )
+        }
+
+        var resultString = ""
+        val stringConversionDuration = measureTimeMillis {
+            resultString = convertToString(factorialResult)
+        }
+
+        uiState.value =
+            UiState.Success(resultString, computationDuration, stringConversionDuration)
     }
+
+    // TODO: execute on background thread
+    private fun convertToString(number: BigInteger): String = number.toString()
 }
