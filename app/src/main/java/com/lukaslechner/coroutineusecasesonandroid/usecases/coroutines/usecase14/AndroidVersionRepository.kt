a226736b3c73061f1431b85b6cfdb777a3c09015
lukaslechner <office@lukaslechner.com>
2020-08-04 15:21:13 +0200

    Remove code in AndroidVersionRepository.kt

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase14/AndroidVersionRepository.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase14/AndroidVersionRepository.kt
@@ -3,9 +3,6 @@ package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase1
 import com.lukaslechner.coroutineusecasesonandroid.mock.AndroidVersion
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
-import timber.log.Timber
 
 class AndroidVersionRepository(
     private var database: AndroidVersionDao,
@@ -18,20 +15,10 @@ class AndroidVersionRepository(
     }
 
     suspend fun loadAndStoreRemoteAndroidVersions(): List<AndroidVersion> {
-        return scope.async {
-            val recentVersions = api.getRecentAndroidVersions()
-                Timber.d("Recent Android versions loaded")
-                for (recentVersion in recentVersions) {
-                    Timber.d("Insert $recentVersion to database")
-                    database.insert(recentVersion.mapToEntity())
-                }
-                recentVersions
-            }.await()
-        }
+        return emptyList()
+    }
 
     fun clearDatabase() {
-        scope.launch {
-            database.clear()
-        }
+
     }
 }
