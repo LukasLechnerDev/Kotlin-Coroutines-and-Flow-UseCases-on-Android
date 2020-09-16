e4bc5060ba1e11543901d0e8f8def498b2fad168
lukaslechner <office@lukaslechner.com>
2020-09-16 11:01:19 +0200

    Improve test for UseCase14

--- app/src/test/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase14/AndroidVersionRepositoryTest.kt
+++ app/src/test/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase14/AndroidVersionRepositoryTest.kt
@@ -1,17 +1,12 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase14
 
-import com.lukaslechner.coroutineusecasesonandroid.mock.AndroidVersion
-import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
-import com.lukaslechner.coroutineusecasesonandroid.mock.VersionFeatures
 import com.lukaslechner.coroutineusecasesonandroid.mock.mockAndroidVersions
-import com.lukaslechner.coroutineusecasesonandroid.utils.EndpointShouldNotBeCalledException
 import com.lukaslechner.coroutineusecasesonandroid.utils.MainCoroutineScopeRule
 import junit.framework.Assert.assertEquals
 import junit.framework.Assert.fail
-import kotlinx.coroutines.*
-import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.runBlockingTest
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -21,17 +16,10 @@ class AndroidVersionRepositoryTest {
     @get: Rule
     val mainCoroutineScopeRule: MainCoroutineScopeRule = MainCoroutineScopeRule()
 
-    private var insertedIntoDb = false
-
-    @Before
-    fun setUp() {
-        insertedIntoDb = false
-    }
-
     @Test
     fun `getLocalAndroidVersions() should return android versions from database`() =
         mainCoroutineScopeRule.runBlockingTest {
-            val fakeDatabase = createFakeDatabase()
+            val fakeDatabase = FakeDatabase()
 
             val repository = AndroidVersionRepository(fakeDatabase, mainCoroutineScopeRule)
             assertEquals(mockAndroidVersions, repository.getLocalAndroidVersions())
@@ -40,8 +28,8 @@ class AndroidVersionRepositoryTest {
     @Test
     fun `loadRecentAndroidVersions() should return android versions from network`() =
         mainCoroutineScopeRule.runBlockingTest {
-            val fakeDatabase = createFakeDatabase()
-            val fakeApi = createFakeApi()
+            val fakeDatabase = FakeDatabase()
+            val fakeApi = FakeApi()
             val repository = AndroidVersionRepository(
                 fakeDatabase,
                 mainCoroutineScopeRule,
@@ -53,54 +41,27 @@ class AndroidVersionRepositoryTest {
     @Test
     fun `loadRecentAndroidVersions() should continue to load and store android versions when calling scope gets cancelled`() =
         mainCoroutineScopeRule.runBlockingTest {
-            val fakeDatabase = createFakeDatabase()
-            val fakeApi = createFakeApi()
+            val fakeDatabase = FakeDatabase()
+            val fakeApi = FakeApi()
             val repository = AndroidVersionRepository(
                 fakeDatabase,
                 mainCoroutineScopeRule,
                 api = fakeApi
             )
 
-            val testScope = TestCoroutineScope(Job())
+            // this coroutine will be executed immediately (eagerly)
+            // how ever, it will stop its execution at the delay(1) in the fakeApi
+            val job = launch {
+                println("running coroutine!")
+                val loadedVersions = repository.loadAndStoreRemoteAndroidVersions()
+                fail("Scope should be cancelled before versions are loaded!")
+            }
 
-            testScope
-                .launch {
-                    println("running coroutine!")
-                    val loadedVersions = repository.loadAndStoreRemoteAndroidVersions()
-                    fail("Scope should be cancelled before versions are loaded!")
-                }
+            job.cancel()
 
-            testScope.cancel()
-
+            // continue coroutine execution after delay(1) in the fakeApi
             advanceUntilIdle()
 
-            assertEquals(true, insertedIntoDb)
+            assertEquals(true, fakeDatabase.insertedIntoDb)
         }
-
-    private fun createFakeApi(): MockApi {
-        return object : MockApi {
-            override suspend fun getRecentAndroidVersions(): List<AndroidVersion> {
-                delay(100)
-                return mockAndroidVersions
-            }
-
-            override suspend fun getAndroidVersionFeatures(apiLevel: Int): VersionFeatures {
-                throw EndpointShouldNotBeCalledException()
-            }
-        }
-    }
-
-    private fun createFakeDatabase(): AndroidVersionDao {
-        return object : AndroidVersionDao {
-            override suspend fun getAndroidVersions(): List<AndroidVersionEntity> {
-                return mockAndroidVersions.mapToEntityList()
-            }
-
-            override suspend fun insert(androidVersionEntity: AndroidVersionEntity) {
-                insertedIntoDb = true
-            }
-
-            override suspend fun clear() {}
-        }
-    }
 }
---
+++ app/src/test/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase14/FakeApi.kt
@@ -0,0 +1,20 @@
+package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase14
+
+import com.lukaslechner.coroutineusecasesonandroid.mock.AndroidVersion
+import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import com.lukaslechner.coroutineusecasesonandroid.mock.VersionFeatures
+import com.lukaslechner.coroutineusecasesonandroid.mock.mockAndroidVersions
+import com.lukaslechner.coroutineusecasesonandroid.utils.EndpointShouldNotBeCalledException
+import kotlinx.coroutines.delay
+
+class FakeApi : MockApi {
+
+    override suspend fun getRecentAndroidVersions(): List<AndroidVersion> {
+        delay(1)
+        return mockAndroidVersions
+    }
+
+    override suspend fun getAndroidVersionFeatures(apiLevel: Int): VersionFeatures {
+        throw EndpointShouldNotBeCalledException()
+    }
+}
