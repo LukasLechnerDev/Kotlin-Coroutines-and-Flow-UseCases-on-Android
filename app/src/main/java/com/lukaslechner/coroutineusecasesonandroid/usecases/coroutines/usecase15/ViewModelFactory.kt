b0e8e2c68a3217d60b0f81ebd01c220d1354790f
Lukas Lechner <office@lukaslechner.com>
2022-06-29 09:16:17 +0200

    Update sdkVersion and dependencies

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase15/ViewModelFactory.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase15/ViewModelFactory.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModelProvider
 class ViewModelFactory(private val context: Context) :
     ViewModelProvider.Factory {
 
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return modelClass.getConstructor(Context::class.java)
             .newInstance(context.applicationContext)
     }
