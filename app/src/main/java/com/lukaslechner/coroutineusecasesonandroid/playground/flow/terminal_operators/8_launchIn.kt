3e2d2b88fa056a56f39209c6ada6303d405390dd
Lukas Lechner <office@lukaslechner.com>
2022-09-08 15:12:32 +0200

    Add playground file for launchIn operator

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/flow/terminal_operators/8_launchIn.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/flow/terminal_operators/8_launchIn.kt
@@ -1,7 +1,12 @@
 package com.lukaslechner.coroutineusecasesonandroid.playground.flow.terminal_operators
 
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import kotlin.coroutines.EmptyCoroutineContext
 
 fun main() {
 
@@ -17,6 +22,24 @@ fun main() {
         emit(2)
     }
 
+    val scope = CoroutineScope(EmptyCoroutineContext)
 
+    flow
+        .onEach { println("Received $it with launchIn() - 1") }
+        .launchIn(scope)
 
+    flow
+        .onEach { println("Received $it with launchIn() - 2") }
+        .launchIn(scope)
+
+    scope.launch {
+        flow.collect {
+            println("Received $it in collect - 1")
+        }
+        flow.collect {
+            println("Receive $it in collect - 2")
+        }
+    }
+
+    Thread.sleep(1000)
 }
