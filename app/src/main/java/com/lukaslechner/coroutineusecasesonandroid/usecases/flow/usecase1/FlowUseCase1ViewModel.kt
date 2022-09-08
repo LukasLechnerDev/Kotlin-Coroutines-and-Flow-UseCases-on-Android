5f5082656cfcf5d5a8c86a000215d6598b5a5532
Lukas Lechner <office@lukaslechner.com>
2022-09-08 15:45:19 +0200

    Use launchIn in UseCase1

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase1/FlowUseCase1ViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase1/FlowUseCase1ViewModel.kt
@@ -3,7 +3,8 @@ package com.lukaslechner.coroutineusecasesonandroid.usecases.flow.usecase1
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 class FlowUseCase1ViewModel(
     stockPriceDataSource: StockPriceDataSource
@@ -12,10 +13,11 @@ class FlowUseCase1ViewModel(
     val currentStockPriceAsLiveData: MutableLiveData<UiState> = MutableLiveData()
 
     init {
-        viewModelScope.launch {
-            stockPriceDataSource.latestStockList.collect { stockList ->
+        stockPriceDataSource
+            .latestStockList
+            .onEach { stockList ->
                 currentStockPriceAsLiveData.value = UiState.Success(stockList)
             }
-        }
+            .launchIn(viewModelScope)
     }
 }
