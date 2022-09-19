eb553cce585a30bc6f09b7aa3b1d47eed0d6877f
Lukas Lechner <office@lukaslechner.com>
2022-09-19 12:21:51 +0200

    Use asLiveData in use case 1

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase1/FlowUseCase1ViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase1/FlowUseCase1ViewModel.kt
@@ -1,31 +1,27 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.flow.usecase1
 
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.asLiveData
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
-import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onStart
+import timber.log.Timber
 
 class FlowUseCase1ViewModel(
     stockPriceDataSource: StockPriceDataSource
 ) : BaseViewModel<UiState>() {
 
-    val currentStockPriceAsLiveData: MutableLiveData<UiState> = MutableLiveData()
-
-    init {
-        stockPriceDataSource
-            .latestStockList
-            .map { stockList ->
-                UiState.Success(stockList) as UiState
-            }
-            .onStart {
-                emit(UiState.Loading)
-            }
-            .onEach { uiState ->
-                currentStockPriceAsLiveData.value = uiState
-            }
-            .launchIn(viewModelScope)
-    }
+    val currentStockPriceAsLiveData: LiveData<UiState> = stockPriceDataSource
+        .latestStockList
+        .map { stockList ->
+            UiState.Success(stockList) as UiState
+        }
+        .onStart {
+            emit(UiState.Loading)
+        }
+        .onCompletion {
+            Timber.tag("Flow").d("Flow has completed.")
+        }
+        .asLiveData()
 }
--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase1/StockPriceDataSource.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase1/StockPriceDataSource.kt
@@ -5,6 +5,7 @@ import com.lukaslechner.coroutineusecasesonandroid.usecases.flow.mock.Stock
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import timber.log.Timber
 
 interface StockPriceDataSource {
     val latestStockList: Flow<List<Stock>>
@@ -14,6 +15,7 @@ class NetworkStockPriceDataSource(mockApi: FlowMockApi) : StockPriceDataSource {
 
     override val latestStockList: Flow<List<Stock>> = flow {
         while (true) {
+            Timber.tag("Flow").d("Fetching current stock prices")
             val currentStockList = mockApi.getCurrentStockPrices()
             emit(currentStockList)
             delay(5_000)
