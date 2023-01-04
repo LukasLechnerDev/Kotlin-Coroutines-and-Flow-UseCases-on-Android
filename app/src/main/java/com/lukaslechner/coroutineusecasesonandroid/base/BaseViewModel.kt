3ee4cb464bc36f25f2fb7c8bc5f3e8e153f21854
minho <my_alsgh@naver.com>
2023-01-04 19:22:33 +0900

    [CHORE] BaseViewModel Not Use for Alone Change Class Open Class to Abstract Class

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/base/BaseViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/base/BaseViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-open class BaseViewModel<T> : ViewModel() {
+abstract class BaseViewModel<T> : ViewModel() {
 
     fun uiState(): LiveData<T> = uiState
     protected val uiState: MutableLiveData<T> = MutableLiveData()
