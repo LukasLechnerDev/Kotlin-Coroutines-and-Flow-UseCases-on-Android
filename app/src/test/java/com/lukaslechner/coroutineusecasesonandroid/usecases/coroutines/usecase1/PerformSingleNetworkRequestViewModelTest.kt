372f212c26a7316e637e710e380d50b735dc7330
Lukas Lechner <l.lechner.extern@rewe-group.at>
2025-12-09 16:09:37 +0100

    Prepare code to record first lesson

---
+++ app/src/test/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase1/PerformSingleNetworkRequestViewModelTest.kt
@@ -0,0 +1,49 @@
+package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase1
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.lukaslechner.coroutineusecasesonandroid.mock.mockAndroidVersions
+import org.junit.After
+import org.junit.Assert
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+
+class PerformSingleNetworkRequestViewModelTest {
+
+    @get: Rule
+    val testInstantTaskExecutorRule: TestRule = InstantTaskExecutorRule()
+
+    private val receivedUiStates = mutableListOf<UiState>()
+
+    @Test
+    fun `should return Success when network request is successful`() {
+
+        // Arrange
+        val fakeApi = FakeSuccessApi()
+        val viewModel = PerformSingleNetworkRequestViewModel(fakeApi)
+
+        observeViewModel(viewModel)
+
+        // Act
+        viewModel.performSingleNetworkRequest()
+
+        // Assert
+        Assert.assertEquals(
+            listOf(
+                UiState.Loading,
+                UiState.Success(mockAndroidVersions)
+            ), receivedUiStates
+        )
+    }
+
+    private fun observeViewModel(viewModel: PerformSingleNetworkRequestViewModel) {
+        viewModel.uiState().observeForever { uiState ->
+            if (uiState != null) {
+                receivedUiStates.add(uiState)
+            }
+        }
+    }
+
+}
